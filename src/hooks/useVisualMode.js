3a3a4bd091821f266af01066f3fa7b91ee72ab0d
Dylan Hamada <dylan.hamada@gmail.com>
2023-12-07 12:47:55 -0500

    Initial commit

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [history, setHistory] = useState([initial]); 
+
+  function transition(newMode, replace = false){
+    if (!replace) {
+      setHistory(prev => [...prev, newMode]);
+    } else {
+      setHistory(prev => {
+        const newHistory = [...prev];
+
+        newHistory[newHistory.length - 1] = newMode;
+
+        return newHistory;
+      });
+    }
+  }
+
+  function back() {
+    setHistory(prev => {
+      if (prev.length > 1) {
+        return [...prev.slice(0, prev.length - 1)]
+      } else {
+        return prev;
+      }
+    })
+  }
+
+  return { mode: history[history.length -1], transition, back };
+}
